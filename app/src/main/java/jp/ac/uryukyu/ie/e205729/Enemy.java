9faf227d0e12663872b04c6f39c6d90e503d91fb
Nanako NakayoshiA <e205729@ie.u-ryukyu.ac.jp>
2020-12-09 00:36:44 +0900

    report3

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e205729/Enemy.java
@@ -0,0 +1,118 @@
+package jp.ac.uryukyu.ie.e205729;
+
+/**
+ * 敵クラス。
+ *  String name; //敵の名前
+ *  int hitPoint; //敵のHP
+ *  int attack; //敵の攻撃力
+ *  boolean dead; //敵の生死状態。true=死亡。
+ * Created by tnal on 2016/11/13.
+ */
+public class Enemy {
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
+
+    /** 以下のフィールドのアクセサ(setter,getter)
+     * String name
+     * int hitPoint
+     * int attack
+     * boolean dead
+    */ 
+
+    /** String nameのsetter
+    */ 
+    public void setName(String name){
+        this.name = name;
+    }
+    /** String nameのgettr
+    */ 
+    public String getName(){
+        return this.name;
+    }
+
+    /** int hitPointのsetter
+    */ 
+    public void setHitPoint(int hitPoint){
+        this.hitPoint = hitPoint;
+    }
+    /** int hitPointのgetter
+    */
+    public int getHitPoint(){
+        return hitPoint;
+    }
+
+    /** int attackのsetter
+    */ 
+    public void setAttack(int attack){
+        this.attack = attack;
+    }
+    /** int attackのgetter
+    */
+    public int getAttack(){
+        return attack;
+    }
+
+    /** boolean deadのsetter
+    */ 
+    public void setDead(boolean dead){
+        this.dead = dead;
+    }
+    /** boolean deadのgetter
+    */ 
+    public boolean isDead(){
+        return dead;
+    }
+
+    
+
+    /**
+     * コンストラクタ。名前、最大HP、攻撃力を指定する。
+     * @param name モンスター名
+     * @param maximumHP モンスターのHP
+     * @param attack モンスターの攻撃力
+     */
+    public Enemy (String name, int maximumHP, int attack) {
+        this.name = name;
+        hitPoint = maximumHP;
+        this.attack = attack;
+        dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+    }
+
+
+    /**
+     * Heroへ攻撃するメソッド。
+     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
+     * @param hero 攻撃対象
+     */
+    public void attack(Hero hero){
+        int damage = (int)(Math.random() * attack);
+
+        if (dead == true){
+            return;
+        }else{
+            hero.wounded(damage);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
+        }
+        
+        
+
+    }
+    
+
+    /**
+     * 自身へ攻撃されたときのダメージ処理をするメソッド。
+     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * @param damage 受けたダメージ
+     */
+    public void wounded(int damage){
+        hitPoint -= damage;
+        if( hitPoint < 0 ) {
+            dead = true;
+            System.out.printf("モンスター%sは倒れた。\n", name);
+        }
+    }
+
+}
